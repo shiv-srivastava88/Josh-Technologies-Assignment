428abad2207c32a52666bd46fcf75e70234742a1
Shivansh Srivastava <shiv-srivastava88>
2020-09-20 00:32:32 +0530

    Updated

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Slider implementation
+
+var slider = tns({
+	container: '.my-slider',
+	items: 1,
+	slideBy: 'page',
+	autoplay: true,
+	navPosition: "bottom",
+	autoplayTimeout:3000,
+	controls:false,
+	autoplayPosition:"bottom",
+	autoplayButtonOutput:false
+});
+
